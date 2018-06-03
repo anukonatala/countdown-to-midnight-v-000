0769585881080e997213cc522f34ed4197414320
Anusha Konatala <anu.konatala@gmail.com>
2018-06-03 18:10:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,12 @@ def countdown(number)
   while number > 0
    puts "#{number} SECOND(S)!"
    number -= 1
-   sleep(5)
+   ountdown_with_sleep
   end
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep
+  sleep(5)
+end
 #countdown(10)
